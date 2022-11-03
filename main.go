44e94f497ae571faf27fdb63ccc737d9404ba422
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-11-03 16:33:26 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ontio/ontology-crypto/keypair"
+	ont "github.com/ontio/ontology-go-sdk"
+	"github.com/ontio/ontology/cmd/utils"
+	"github.com/ontio/ontology/common"
+	"github.com/ontio/ontology/common/log"
+	"github.com/ontio/ontology/core/types"
+)
+
+func multiSigTransfer() {
+	m := 5
+	var wallets []string
+	wallets = []string{
+		"./wallet1.dat",
+		"./wallet2.dat",
+		"./wallet3.dat",
+		"./wallet4.dat",
+		"./wallet5.dat",
+		"./wallet6.dat",
+		"./wallet7.dat",
+	}
+	toAddr := "AYMnqA65pJFKAbbpD8hi5gdNDBmeFBy5hS"
+
+	accs := make([]*ont.Account, 0)
+	pks := make([]keypair.PublicKey, 0, len(wallets))
+	for _, w := range wallets {
+		a, err := loadWallet(w, "pwd")
+		if err != nil {
+			log.Errorf("load wallet error: %s", err)
+			return
+		}
+		accs = append(accs, a)
+		pks = append(pks, a.PublicKey)
+		log.Infof("addr:%s", a.Address.ToBase58())
+	}
+	payer, err := types.AddressFromMultiPubKeys(pks, int(m))
+	if err != nil {
+		log.Errorf("AddressFromMultiPubKeyserr:%s", err)
+		return
+	}
+	balance, err := utils.GetBalance(payer.ToBase58())
+	// if balance == nil {
+	// 	log.Infof("payer:%s ont:%d", payer.ToBase58(), 0)
+	// } else {
+	// 	log.Infof("payer:%s ont:%d", payer.ToBase58(), balance.Ont)
+	// }
+	// // payerOnt, err := strconv.Atoi(balance.Ont)
+
+	// toBalance, err := utils.GetBalance(toAddr)
+	// if err != nil {
+	// 	log.Errorf("addr from base 58 err:%s", err)
+	// }
+	// log.Infof("to:%s ont:%s, payerOnt:%d", toAddr, toBalance.Ont, payerOnt)
+
+	txhash, err := sdkMultiTransfer(0, 30000, accs, uint16(m), "ont", payer, toAddr, 200000000)
+	if err != nil {
+		log.Errorf("err:%s", err)
+		return
+	}
+	log.Infof("hash: %x", txhash)
+
+}
+
+func sdkMultiTransfer(gasPrice, gasLimit uint64, signers []*ont.Account, m uint16, asset string, from common.Address, to string, amount uint64) (string, error) {
+	pks := make([]keypair.PublicKey, 0, len(signers))
+	for _, signer := range signers {
+		pks = append(pks, signer.PublicKey)
+	}
+
+	sdk := ont.NewOntologySdk()
+
+	sdk.NewRpcClient().SetAddress("http://10.0.1.128:20336")
+
+	mutableTx, err := utils.TransferTx(0, gasLimit, asset, from.ToBase58(), to, amount)
+	if err != nil {
+		return "", err
+	}
+
+	for _, signer := range signers {
+		sdk.MultiSignToTransaction(mutableTx, m, pks, signer)
+		fmt.Printf("sign\n")
+	}
+	hash, err := sdk.SendTransaction(mutableTx)
+	if err != nil {
+		return "", err
+	}
+	return hash.ToHexString(), nil
+}
+
+func loadWallet(wallet, pwd string) (*ont.Account, error) {
+	clientImpl, err := ont.OpenWallet(wallet)
+	if clientImpl == nil {
+		log.Errorf("clientImpl is nil")
+		return nil, err
+	}
+	a, err := clientImpl.GetDefaultAccount([]byte(pwd))
+	if a == nil {
+		log.Errorf("acc is nil")
+		return nil, err
+	}
+	return a, nil
+}
+
+func main() {
+	multiSigTransfer()
+}
